40800bd9d926a359a9650710cf33e5bc4102f866
quiLV <quilevan1234abc@gmail.com>
2024-11-30 02:56:16 +0700

    Hoàn_thành

---
+++ quiLV_ALTA_Class/Model/Calendar.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace quiLV_ALTA_Class.Model
+{
+    public class Calendar //Lịch giảng dạy
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Class_Id { get; set; }
+        public int Subject_ID { get; set; }
+        public TimeSpan Study_Time {  get; set; }
+        public int ClassRoom {  get; set; }
+        public DateTime Time { get; set; }
+        public int Rank { get; set; }
+        public int Teacher_Id { get; set; }
+
+    }
+}
